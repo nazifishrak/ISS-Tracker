b21ce596dc17fcf9a2d0768b00038ba3e4666633
Nazif <nzfishrak60@gmail.com>
2022-08-10 11:49:35 +0600

    added iss tracking features

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import smtplib
 import requests
 from datetime import datetime
 
@@ -19,23 +20,29 @@ def is_near():
     if (MY_LAT-5 <= iss_latitude <= MY_LAT+5) and (MY_LONG-5 <= iss_longitude < MY_LONG+5):
         return True
 
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+    response = requests.get(
+        "https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-response = requests.get(
-    "https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour()
 
-time_now = datetime.now()
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
-# If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+
+
+
+if is_near() and is_night():
+    connection = smtplib.SMTP("smtp.gmail.com", port= 587)
+    connection.starttls()
+    connection.sendmail()
